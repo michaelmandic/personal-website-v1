b096e53a88857c184876f359a7ea01560328b982
Michael Mandic <86262609+michaelmandic@users.noreply.github.com>
2025-05-22 23:46:13 +0200

    Added 404 page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
+  appType: 'mpa',
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, '404.html'),
+      },
+    },
+  },
 })
