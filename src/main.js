af0d7a5a8b9d261e170ae98bd779f544ee84b0a4
Michael Mandic <86262609+michaelmandic@users.noreply.github.com>
2025-05-22 23:05:01 +0200

    Changed hero link hover transition and fixed pointer interaction with hidden header

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ document.addEventListener("DOMContentLoaded", () => {
         const triggerPoint = window.innerHeight*0.8;
 
         if (window.scrollY > triggerPoint) {
-            header.classList.remove('opacity-0');
+            header.classList.remove('opacity-0', 'pointer-events-none');
         } else {
-            header.classList.add('opacity-0');
+            header.classList.add('opacity-0', 'pointer-events-none');
         }
     });
 });
