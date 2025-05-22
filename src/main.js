270630399a06fdf0b4c6e2ba03ef95cce0eaf8b6
Michael Mandic <86262609+michaelmandic@users.noreply.github.com>
2025-05-23 01:57:49 +0200

    Added scrolling inidactor arrow

--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
     const header = document.getElementById('header');
+    const scrollArrow = document.getElementById('scroll-arrow');
 
     window.addEventListener('scroll', () => {
-        const triggerPoint = window.innerHeight*0.8;
+        const triggerPointHeader = window.innerHeight*0.8;
+        const triggerPointScrollArrow = window.innerHeight*0.2;
 
-        if (window.scrollY > triggerPoint) {
+        if (window.scrollY > triggerPointHeader) {
             header.classList.remove('opacity-0', 'pointer-events-none');
         } else {
             header.classList.add('opacity-0', 'pointer-events-none');
+        }
+
+        if (window.scrollY > triggerPointScrollArrow) {
+            scrollArrow.classList.add('opacity-0', 'pointer-events-none');
+        } else {
+            scrollArrow.classList.remove('opacity-0', 'pointer-events-none');
         }
     });
 });
