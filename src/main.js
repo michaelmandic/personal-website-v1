671043dc3ea6b6fac78f518557c108498221c30e
Michael Mandic <86262609+michaelmandic@users.noreply.github.com>
2025-05-30 02:39:45 +0200

    Changed a lot of styling and added /links page

--- src/main.js
+++ src/main.js
@@ -1,21 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const header = document.getElementById('header');
-    const scrollArrow = document.getElementById('scroll-arrow');
 
-    window.addEventListener('scroll', () => {
-        const triggerPointHeader = 1;
+    const header = document.querySelector('#header');
+    const scrollArrow = document.querySelector('#scroll-arrow');
+
+    console.log(document.querySelector('.header'));
+    console.log(document.querySelector('.scroll-arrow'));
+
+    if (header) {
+        
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 1) {
+                header.classList.add('outline-[1px]', 'outline-(--transparent-white)', 'h-[4rem]');
+                header.classList.remove('h-[5rem]')
+            } else {
+                header.classList.remove('outline-[1px]', 'outline-(--transparent-white)', 'h-[4rem]');
+                header.classList.add('h-[5rem]')
+            };
+        });
+    };
+
+    if (scrollArrow) {
+        
         const triggerPointScrollArrow = window.innerHeight*0.2;
-
-        if (window.scrollY > triggerPointHeader) {
-            header.classList.add('outline-[1px]', 'outline-(--transparent-white)');
-        } else {
-            header.classList.remove('outline-[1px]', 'outline-(--transparent-white)');
-        }
-
         if (window.scrollY > triggerPointScrollArrow) {
             scrollArrow.classList.add('opacity-0');
         } else {
             scrollArrow.classList.remove('opacity-0');
-        }
-    });
+        };
+    };
+
 });
--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         404: resolve(__dirname, '404.html'),
+        links: resolve(__dirname, 'links.html'),
       },
     },
   },
